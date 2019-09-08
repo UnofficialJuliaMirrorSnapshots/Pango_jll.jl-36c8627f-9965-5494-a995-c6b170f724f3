aeee2cc9bb113f06615f8578396e6a51e9415f69
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-08 09:07:09 +0000

    Snapshot of branch master taken on 2019-09-08T05:07:08.506-04:00 from "https://github.com/UnofficialJuliaMirror/Pango_jll.jl-36c8627f-9965-5494-a995-c6b170f724f3"

---
+++ src/Pango_jll.jl
@@ -0,0 +1,31 @@
+module Pango_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("36c8627f-9965-5494-a995-c6b170f724f3"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "Pango", artifacts_toml) for e in artifacts["Pango"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load Pango; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module Pango_jll
